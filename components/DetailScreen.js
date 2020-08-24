a58f11078b58f87515bee76f5553b21acfee37c6
anand100b <57651518+anand100b@users.noreply.github.com>
2020-08-24 18:27:17 +1000

    new page

---
+++ components/DetailScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+export const DetailScreen = ( props ) => {
+  return (
+    <View>
+      <Text>
+        Detail Page
+      </Text>
+    </View>
+  )
+}
