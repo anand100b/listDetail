1d12564c79ca13a8619dfa005bf9414ab9bad11d
anand100b <57651518+anand100b@users.noreply.github.com>
2020-09-14 18:07:54 +1000

    styles added

--- components/Select.js
+++ components/Select.js
@@ -7,21 +7,60 @@ export const Select = (props) => {
 
   const Items = props.items.map((item,index)=>{
 return (
-  <TouchableOpacity key={index} onPress={()=>{setSelected(item.value) }} >
+  <TouchableOpacity
+  style={selectStyles.selectItem} 
+   key={index} onPress={()=>{setSelected(item.value) 
+    setVisible(false)
+   }} >
     <Text>{item.label}</Text>
   </TouchableOpacity>
 )
   })
 
   return (
-    <View>
-      <TouchableOpacity>
+    <View style={selectStyles.selectView}>
+      <TouchableOpacity onPress={setVisible(true)}>
         <Text>{Selected}</Text>
         <Image
         style={selectedStyles.selectImage}
         source={require('../assets/drop-down-round-button.png')}
         />
       </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        visible = {visible}
+      >
+        <View style={selectStyles.modalView}>
+          <ScrollView>
+            {Items}
+          </ScrollView>
+        </View>
+      </Modal>
     </View>
   )
 }
+
+const selectStyles = StyleSheet.create({
+  selectView: {
+    padding: 10,
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  selectImage: {
+    width: 15,
+    height: 15,
+    position: 'absolute',
+    right: 3,
+    top: 3,
+  },
+  selectItem: {
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    borderBottomColor: '#dddddd',
+    borderBottomWidth: 1,
+  },
+  modalView: {
+    marginTop: 100,
+  },
+})
